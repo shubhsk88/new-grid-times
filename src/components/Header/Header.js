2a350c996a4c8cc97049ae589bcd16f4628c8eab
Shubham <shubham88singh@gmail.com>
2022-01-05 21:10:25 +0530

    Doing the first part

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -21,10 +21,17 @@ const Header = () => {
               <Menu size={24} />
             </button>
           </ActionGroup>
+          <LogoWrapper>
+            <Logo />
+          </LogoWrapper>
           <ActionGroup>
-            <button>
+            <ProfileButton>
               <User size={24} />
-            </button>
+            </ProfileButton>
+            <RightHeader>
+              <Button>SUBSCRIBE</Button>
+              <a href="sdsa">Already a subscriber?</a>
+            </RightHeader>
           </ActionGroup>
         </Row>
       </SuperHeader>
@@ -38,14 +45,45 @@ const Header = () => {
 const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
+
   color: white;
+  @media ${QUERIES.laptopAndUp} {
+    background: transparent;
+    color: var(--color-gray-900);
+  }
 `;
 
+const ProfileButton = styled.button`
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
+`;
+const LogoWrapper = styled.div`
+  display: none;
+  @media ${QUERIES.laptopAndUp} {
+    display: block;
+  }
+`;
 const Row = styled(MaxWidthWrapper)`
   display: flex;
   justify-content: space-between;
+  @media ${QUERIES.laptopAndUp} {
+    align-items: center;
+  }
 `;
 
+const RightHeader = styled.div`
+  display: none;
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    a {
+      text-decoration: underline;
+      font-style: italic;
+    }
+  }
+`;
 const ActionGroup = styled.div`
   display: flex;
   gap: 24px;
@@ -65,6 +103,9 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 export default Header;
